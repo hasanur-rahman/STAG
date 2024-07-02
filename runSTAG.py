0fb9c469ecc5653e6f6d2ec712bd743fa630c097
hasanur-rahman <rahman.md.hasanur7@gmail.com>
2024-07-02 14:29:57 -0500

    add

--- runSTAG.py
+++ runSTAG.py
@@ -17,15 +17,13 @@ from openai import OpenAI
 # from bs4 import BeautifulSoup
 
 
-# openai.api_key = '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'
-# tts_api_key = ''
 
 
 # client = OpenAI()
 
 class CultureAndLanguageModule:
     def __init__(self, destination):
-        chatgpt_api_key = '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'
+        chatgpt_api_key = 'placeholder_api_key'
         # chatgpt_text_messages = chatgpt_msg
         self.client = OpenAI(api_key = chatgpt_api_key)
         self.destination = destination
@@ -229,7 +227,7 @@ class VirtualCityExplorationModule:
 
 class HistoricalOverviewModule:
     def __init__(self, destination):
-        chatgpt_api_key = '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'
+        chatgpt_api_key = 'placeholder_api_key'
         self.client = OpenAI(api_key= chatgpt_api_key)
         self.destination = destination
         
@@ -331,7 +329,7 @@ class PersonalizedItineraryGenerator:
 
     def __init__(self, user_preferences, current_location, destination, transportation_mode):
         # openai.api_key = self.GPT_API_KEY
-        chatgpt_api_key = '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'
+        chatgpt_api_key = 'placeholder_api_key'
         self.client = OpenAI(api_key= chatgpt_api_key)
         # self.client = OpenAI()
         self.user_preferences = user_preferences
@@ -416,121 +414,9 @@ class UtilityModule:
 
 # User Interface
 def main():
-    # Instantiate modules
-    # api_key = 'your-api-key'  # Replace with your OpenAI API key
-    # culture_language_module = CultureAndLanguageModule(api_key)
-    # culture_language_module = CultureAndLanguageModule()
-    # openai_key = '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'
-    # gmaps_key = 'AIzaSyAzKG2cNwaXnqgNh8MzWRqCPMVTKsCQPQc'
-    # city_exploration_module = VirtualCityExplorationModule(cityName, openai_key, gmaps_key, NumberofAttractions=5)
-    # historical_overview_module = HistoricalOverviewModule()
-    # itinerary_generator = PersonalizedItineraryGenerator()
-
-    # chatgpt_text_messages = [
-    #     {
-    #         "role": "system",
-    #         "content": ""
-    #     }
-    # ]
+    
     pass
 
-    # # User inputs
-    # # destination = input("Enter your destination: ")
-    # # city_exploration_module = VirtualCityExplorationModule(destination, openai_key, gmaps_key)
-    # # city_exploration_module.generateAttractionsImage()
-
-    # # preferences = {
-    # #     "interests": input("Enter your interests (comma-separated): ").split(","),
-    # #     "dietary_restrictions": input("Enter your dietary restrictions: "),
-    # #     "budget": float(input("Enter your budget: ")),
-    # # }
-    # # landmark_info = input("Enter landmark information: ")
-    # # transportation_mode = input("Enter preferred transportation mode: ")
-    # current_location = "Chicago"
-    # destination = "Madrid"
-    # preferences = {
-    #     "interests": ["Sport"],
-    #     "dietary_restrictions": "Halal",
-    #     "budget": 1000,
-    #     "number_of_days": 7
-    # }
-    # # landmark_info = "CampNou"
-    # transportation_mode = "Driving"
-
-    # # Instantiate modules
-    
-    # gmaps_key = 'AIzaSyAzKG2cNwaXnqgNh8MzWRqCPMVTKsCQPQc'
-    # set_api_key("3af892a1fb2875dfe482a456441d6ea4")
-
-    # utility_module = UtilityModule()
-    
-
-
-    # print("\n\nVirtual City Exploration.")
-    # city_exploration_module = VirtualCityExplorationModule(destination, gmaps_key)
-    # city_exploration_module.generateAttractionsImage()
-    
-    # culture_language_module = CultureAndLanguageModule(destination)
-    # cultural_insights = culture_language_module.get_cultural_insights()
-    # print("\n\n### Cultural Insights ###")
-    # print(cultural_insights)
-    # common_phrases = culture_language_module.get_common_phrases()
-    # print("\n\n### Common Phrases ###")
-    # print(common_phrases)
-    # culture_language_module.get_audio_pronunciations(common_phrases)
-    # culture_language_module = CultureAndLanguageModule(destination)
-    # cultural_insights = culture_language_module.get_cultural_insights()
-    # print("\n\n### Cultural Insights ###")
-    # print(cultural_insights)
-    # common_phrases = culture_language_module.get_common_phrases()
-    # print("\n\n### Common Phrases ###")
-    # print(common_phrases)
-    # culture_language_module.get_audio_pronunciations(common_phrases)
-
-    # historical_overview_module = HistoricalOverviewModule(destination)
-    # historical_overview_result,historical_overview_result_list = historical_overview_module.generate_historical_overview()
-    # print("\n\n### Historical Overview ###")
-    # print(historical_overview_result)
-    # image_prompts_list = historical_overview_module.generate_imagepromt_urllist_basedon_historical_overview(historical_overview_result_list)
-    # print("\n\n### Image Prompts List ###")
-    # image_urls = []
-    # image_number = 1
-    # for historical_overview, image_prompt in image_prompts_list:
-    #     print(image_prompt)
-    #     image_url = historical_overview_module.generate_image_from_image_prompt(image_prompt)
-    #     # print("image url: ", image_url)
-    #     image_urls.append(image_url)
-    #     utility_module.save_image_from_url(image_url, str(image_number), destination.replace(' ', '-'), "HistoricalOverviewImages")
-    #     image_number = image_number + 1
-
-
-        
-
-    # # print("\n\nShowing Images")
-    # # for url in image_urls:
-    # #     utility_module.open_image_from_url(url)
-
-    # itinerary_generator = PersonalizedItineraryGenerator(preferences, current_location, destination)
-    # personalized_itinerary = itinerary_generator.generate_personalized_itinerary()
-    # print("\n### Personalized Itinerary ###")
-    # print(personalized_itinerary)
-    # itinerary_generator = PersonalizedItineraryGenerator(preferences, current_location, destination)
-    # personalized_itinerary = itinerary_generator.generate_personalized_itinerary()
-    # print("\n### Personalized Itinerary ###")
-    # print(personalized_itinerary)
-
-    
-
-    
-
-
-# Sample inputs
-# Enter your destination: Paris
-# Enter your interests (comma-separated): History,Art
-# Enter your dietary restrictions: Vegetarian
-# Enter your budget: 1000
-# Enter landmark information: Eiffel Tower
-# Enter preferred transportation mode: Walking
 
 if __name__ == "__main__":
     main()
